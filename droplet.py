65726e18f128f571a0cff7e5775ec7e1fa9c3e33
ChenSmallX <641751205@qq.com>
2018-08-05 17:48:25 +0800

    droplet class: use @property

--- droplet.py
+++ droplet.py
@@ -3,26 +3,48 @@
 
 
 class droplet(object):
+    '''class to store information of server
+
+    '''
+    # set a limitation to droplet jest can have 3 attribution
+    __slot__ = ('__name', '__ip', '__password', 'information')
 
     def __init__(self, name, ip, password):
-        self.__name = name
-        self.__ip = ip
-        self.__password = password
+        if isinstance(name, str):
+            self.__name = name
+        else:
+            print('the name "', name, '" is not a str')
+        if isinstance(ip, str):
+            self.__ip = ip
+        else:
+            print('the ip "', ip, '" is not a str')
+        if isinstance(password, str):
+            self.__password = password
+        else:
+            print('the password "', password, '" is not a str')
 
-    def setName(self, name):
-        self.__name = name
-
-    def getName(self):
+    @property
+    def name(self):
         return self.__name
 
-    def setIP(self, ip):
-        self.__ip = ip
+    @name.setter
+    def name(self, name):
+        self.__name = name
 
-    def getIP(self):
+    @property
+    def ip(self):
         return self.__ip
 
-    def setPassword(self, password):
+    @ip.setter
+    def ip(self, ip):
+        self.__ip = ip
+
+    @property
+    def password(self):
+        return self.__password
+
+    @password.setter
+    def password(self, password):
         self.__password = password
 
-    def getPassword(self):
-        return self.__password
+    
