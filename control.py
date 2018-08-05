b3cca94b29efa8375fede178928dcfe6c293bf4b
ChenSmallX <641751205@qq.com>
2018-08-05 17:53:09 +0800

    适应修改过的 droplet.py

--- control.py
+++ control.py
@@ -1,11 +1,11 @@
-#!/anaconda3/bin/python python
+#!/anaconda3/bin/ python
 # -*- coding: utf-8 -*-
 
 from droplet import droplet
 import shell
 
 
-def getServerInfo(strInfo):
+def get_server_info(strInfo):
     """
     get name, ip and password from a string 
     info which from ping information
@@ -22,40 +22,42 @@ def getServerInfo(strInfo):
     return name, ip, password
 
 
-def getServers(fServers):
+def get_servers(f_servers):
     servers = []
 
-    strInfo = fServers.readline()
+    strInfo = f_servers.readline()
     while strInfo:
-        name, ip, password = getServerInfo(strInfo)
+        name, ip, password = get_server_info(strInfo)
         tempDroplet = droplet(name, ip, password)
         servers.append(tempDroplet)
 
-        strInfo = fServers.readline()  # 这一步要求 servers.txt 文件要有最后一行空行
+        strInfo = f_servers.readline()  # 这一步要求 servers.txt 文件要有最后一行空行
 
     return servers
 
 
-def printServer(server):
-    print(server.getName())
-    print(server.getIP())
-    print(server.getPassword())
+def print_server(server):
+    print(server.name)
+    print(server.ip)
+    print(server.password)
 
 
-def testGroupPingOnce(servers):
+def test_group_ping_once(servers):
     for ser in servers:
-        print("ping:", ser.getName(), ":", end='')
+        print("ping:", ser.name, ":", end='')
         time = shell.ping(ser)  # maybe reture time as a float or s"timeout"
         print(time)
 
 
 if __name__ == "__main__":
     # Watch-Server-Dog/ (when debug, contact it before the path)
-    fServers = open("server.txt", "r")
-    servers = getServers(fServers)
+    f_servers = open("server.txt", "r")
+    servers = get_servers(f_servers)
 
     for ser in servers:
-        printServer(ser)
+        print_server(ser)
         print("")
 
-    testGroupPingOnce(servers)
+    test_group_ping_once(servers)
+
+    f_servers.close()
--- shell.py
+++ shell.py
@@ -6,15 +6,64 @@ import os
 
 
 def ping(server):
-    pingInfo = os.popen("ping " + server.getIP() + " -c 1", "r")
-    info0 = pingInfo.readline()
-    info0 = pingInfo.readline()
+    '''return the ping value one time
+    argument: server (droplet class)
+    return: time (would be a float or a s"timeout")
 
+    cause: The ping commend can return a series of 
+    information about the network connection status
+    to target server.
+    use the argument "-c" and attach one number 
+    after that can do the ping for a certain times 
+    of the number.
+    We get the 2nd line of the returned information 
+    (because the 1st line is a prompt) to extract the 
+    time of ping.
+    '''
+    ping_info = os.popen("ping " + server.ip + " -c 1", "r")
+    info0 = ping_info.readline()
+    info0 = ping_info.readline()
+
+    # cause the ping commend would be timeout,
+    # so when exception, must be ping timeout
     try:
-        strTime = info0[info0.index("time=")+5: info0.index(" ms")]
+        str_time = info0[info0.index("time=")+5: info0.index(" ms")]
     except BaseException:
         time = "timeout"
     else:
-        time = float(strTime)
+        time = float(str_time)
 
     return time
+
+
+def get_shell_size():
+    '''get lines and columns of current shell 
+    return: lines, columns
+
+    cause: In unix/linux shell, the commend "echo 
+    $LINES" and "echo $COLUMNS" can return the 
+    size of this shell
+    '''
+    lines = int(os.popen("echo $LINES", "r").readline())
+    columns = int(os.popen("echo $COLUMNS", "r").readline())
+    return lines, columns
+
+
+def get_location_info(server):
+    '''
+    argument: server (droplet class)
+    return: location, isp
+
+    cause: post to "cip.cc" can return the information 
+    of the ip location which attach after the url
+    '''
+    str_info = os.popen("curl cip.cc/" + server.ip, "r")
+    index1 = str_info.index("地址    : ")+len("地址    : ")
+    index2 = str_info.index("运营商  : ")-1
+    str_location = str_info[index1: index2]
+
+    index1 = str_info.index("运营商  : ")+len("运营商  : ")
+    index2 = str_info.index("数据二  : ")-2
+    str_isp = str_info[index1: index2]
+
+    return str_location, str_isp
